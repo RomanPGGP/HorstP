6884edd73282ccd5814c740de6dd1feeb349d26b
Román PG <roman.perez.g94@gmail.com>
2016-11-17 17:02:28 -0600

    Update .objdeps.mk

--- .objdeps.mk
+++ .objdeps.mk
@@ -14,7 +14,7 @@ hutil.o: hutil.c hutil.h
 ieee80211_duration.o: ieee80211_duration.c main.h ieee80211_duration.h
 listsort.o: listsort.c listsort.h
 main.o: main.c main.h hutil.h network.h control.h conf_options.h \
- ieee80211_duration.h protocol_parser.h
+ ieee80211_duration.h protocol_parser.h display.h
 network.o: network.c main.h network.h
 protocol_parser.o: protocol_parser.c olsr_header.h batman_header.h \
  batman_adv_header-14.h main.h hutil.h
